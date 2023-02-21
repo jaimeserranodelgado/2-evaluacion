b84eb4f1bdaa065b6c6eeae09ead81dac16cdee4
Jaime Serrano Delgado <113890867+jaimeserranodelgado@users.noreply.github.com>
2023-02-21 10:30:42 +0100

    Add files via upload

---
+++ sin nombre 13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+//Este programa ordena por el metodo de la burbuja
+using namespace std;
+int main(){
+	//Reservo memoria para un vector de numeros enteros
+	//sin repeticion
+	int numeros[5];
+	//Leemos los 5 numeros
+	int cont;
+	for(cont=0;cont<5;cont++){
+		printf("dime un numero: ");
+		scanf("%s",&numeros[cont]);
+	}
+	printf("\nLa lista desordenada es : ");
+	for(cont=0;cont<5;cont++))
+}
